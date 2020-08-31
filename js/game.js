304fd81b6b2334c209ed675ea786dd5170ed0054
ankitkm21 <30433288+ankitkm21@users.noreply.github.com>
2020-08-31 19:37:45 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -44,7 +44,7 @@ class Game {
       index = index + 1;
       x = 500 - allPlayers[plr].distance;
       y = 500;
-      players[]={};
+      //players[]={};
       players[index - 1].x = x;
       players[index - 1].y = y;
 
